dccda78e6ee8b50ac1a146ce959385323e3e76c6
idris-CGS <idris.yousfi@contentgardening.com>
2023-11-30 12:25:33 +0100

    Add middleware & redirect unauthorized user to login page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 
+from src import router as template_router
 from src.database import create_db_and_tables
 from src.auth import router as auth_router
 from src.projects import router as project_router
-from src import router as template_router
-
+from src.exceptions import RequiresLoginException
 from src.config import (
     DEBUG,
     PROJECT_NAME,
@@ -22,6 +23,48 @@ app = FastAPI(
     version=API_VERSION,
     debug=DEBUG,
 )
+
+
+# redirection block
+@app.exception_handler(RequiresLoginException)
+async def exception_handler(request: Request, exc: RequiresLoginException):
+    """this handler allows to route the login exception to the login page."""
+    print("handle redirect exception")
+    return RedirectResponse(url="/login")
+
+
+@app.middleware("http")
+async def create_auth_header(
+    request: Request,
+    call_next,
+):
+    """
+    Check if there are cookies set for authorization. If so, construct the
+    Authorization header and modify the request (unless the header already
+    exists!)
+    """
+    if "Authorization" not in request.headers and "access_token" in request.cookies:
+        access_token = request.cookies["access_token"]
+
+        request.headers.__dict__["_list"].append(
+            (
+                "authorization".encode(),
+                f"Bearer {access_token}".encode(),
+            )
+        )
+    elif (
+        "Authorization" not in request.headers and "access_token" not in request.cookies
+    ):
+        request.headers.__dict__["_list"].append(
+            (
+                "authorization".encode(),
+                f"Bearer 12345".encode(),
+            )
+        )
+
+    response = await call_next(request)
+    return response
+
 
 app.include_router(auth_router.router, prefix="/api")
 app.include_router(project_router.router, prefix="/api")
